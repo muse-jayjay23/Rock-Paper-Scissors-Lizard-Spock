b06b1d37467b8f6b85ba0112ce16590d91a66cd4
muse-jayjay23 <joshuamjones23@hotmail.com>
2022-11-04 10:48:35 +0000

    Add to win function to display to both user/comp spans and result h2 elements when called

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 // Initialising all the elements of the html page as variables in JS at top of the page. Help load times.
 
 // scores for user and comp
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 // show this back to the page
 const userScoreSpan = document.getElementById("user-score");
 const computerScoreSpan = document.getElementById("computer-score");
 // retrieve other elements from html
 const scoreBoardDiv = document.querySelector(".score-board");
-const resultDiv = document.querySelector(".result-notification");
+const resultH2 = document.querySelector(".result-notification > h2");
 // user choices
 const rockDiv = document.getElementById("rock");
 const paperDiv = document.getElementById("paper");
@@ -25,6 +25,28 @@ const scissorsDiv = document.getElementById("scissors");
 }
 
 /**
+ * Seperate functions for the win, lose draw options of the game, setting what will happen for each
+ */
+function win(user, comp) {
+// Increments user score
+    userScore++;
+// Updates the score span element
+    userScoreSpan.innerHTML = userScore;
+    computerScoreSpan.innerHTML = computerScore;
+// Displays to the result div h2 element the feedback of the result when we win.
+    resultH2.innerHTML = `${user} beats ${comp} , well done, you win!`;
+}
+
+function lose() {
+    console.log("lose");
+// Increments computer score
+    computerScore++;
+}
+
+function draw() {
+    console.log("draw");
+}
+/**
  * This will take the users choice when clicked so it can be compared against the computers choice
  */
 function game(userChoice) {
@@ -34,19 +56,19 @@ function game(userChoice) {
         case "rockscissors":
         case "scissorspaper":
         case "paperrock":
-            console.log("user wins");
+            win(userChoice, computerChoice);
             break;
 // cases where user loses
         case "rockpaper":
         case "scissorsrock":
         case "paperscissors":
-            console.log("computer wins");
+            lose(userChoice, computerChoice);
             break;
 // cases where there is a draw
         case "rockrock":
         case "scissorsscissors":
         case "paperpaper":
-            console.log("draw!");
+            draw(userChoice, computerChoice);
             break;
     }
 }
